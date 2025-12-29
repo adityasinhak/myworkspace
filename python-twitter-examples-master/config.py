419d7e110c4c73a52b5aa1a8d53fb29a636b301c
Aditya Kumar Sinha <sinhakaditya@gmail.com>
2025-12-29 08:29:14 +0530

    Update config.py

--- python-twitter-examples-master/config.py
+++ python-twitter-examples-master/config.py
@@ -4,7 +4,7 @@
 #	https://apps.twitter.com/app/new
 #--------------------------------------------------------------------------------
 
-consumer_key = "KdeYJjZwH5cAT2BhMhnYrTX8P"
-consumer_secret = "ZCTwFKduYbdkhPROeRqwTe3o1V2qWCfitXmjCqQw5czlrbiKEW"
-access_key = "800267514013884416-5zLfj7cFYgKeXN9CxeqAalYBYrzDmXT"
-access_secret = "wqoHu3ukdGAq1wwVcrDy4hD5k4NSTvbS0jG0uFuTNbZDp"
+consumer_key = ""
+consumer_secret = ""
+access_key = "-5zLfj7cFYgKeXN9CxeqAalYBYrzDmXT"
+access_secret = ""
